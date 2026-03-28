74202597bd32c225bdb44bc221b51db1b61b12bd
kangtongshun <72067214+kangtongshun@users.noreply.github.com>
2026-03-28 15:00:17 +0800

    Log request body on receiving POST message

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.post('/api', async (req, res) => {
     console.log("=".repeat(50));
     console.log("收到POST消息推送");
     console.log("请求时间:", new Date().toISOString());
-    
+    console.log(req.body)
     // 获取URL参数并进行Urldecode处理
     let timestamp = req.query.timestamp;
     let nonce = req.query.nonce;
